14b7223c6896969a16e44a0eb859d771e1cbe4ca
Предеин Никита <ne-kit@outlook.com>
2020-06-29 00:55:45 +0500

    Run server

--- pigeon-server/Program.cs
+++ pigeon-server/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
@@ -10,11 +12,13 @@ namespace pigeon_server
 {
     class Program
     {
+        public static NetworkStream NetworkStream { get; private set; }
+
         static void Main(string[] args)
         {
             string Welcome = "\r\n  =================================\r\n" +
                              "    WELCOME TO PIGEON FILE SENDER\r\n" +
-                             "  =================================";
+                             "  =================================\r\n\r\n";
             foreach (char c in Welcome)
             {
                 Console.Write(c);
@@ -23,8 +27,32 @@ namespace pigeon_server
             Console.ReadKey();
             // start listener
 
-            TcpListener TcpListener = new TcpListener(55387);
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 55387;
+            TcpListener tcpListener = new TcpListener(ip, port);
+            tcpListener.Start();
 
+            Boolean isConnected = false;
+
+            while (true)
+            {
+                if (!isConnected)
+                {
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                    NetworkStream ns = tcpClient.GetStream();
+                    ns.ReadTimeout = 36000;
+                    ns.WriteTimeout = 36000;
+                    StreamReader sr = new StreamReader(ns);
+                    StreamWriter sw = new StreamWriter(ns);
+                    sw.AutoFlush = true;
+                    isConnected = true;
+                    Console.Clear();
+                    Console.Write(" [ Connected ] \r\n");
+                    break;
+                }
+            }
+
+            Console.WriteLine();
 
         }
     }
