fc3a948186f72b339fee666185d99738a40b329f
Предеин Никита <ne-kit@outlook.com>
2020-06-29 00:36:10 +0500

    Добавьте файлы проекта.

---
+++ pigeon-server/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace pigeon_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Welcome = "\r\n  =================================\r\n" +
+                             "    WELCOME TO PIGEON FILE SENDER\r\n" +
+                             "  =================================";
+            foreach (char c in Welcome)
+            {
+                Console.Write(c);
+                Thread.Sleep(5);
+            }
+            Console.ReadKey();
+            // start listener
+
+
+        }
+    }
+}
