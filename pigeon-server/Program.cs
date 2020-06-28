909d6bdabe8b683c1a3f3fa59e13b35d97275418
Предеин Никита <ne-kit@outlook.com>
2020-06-29 00:42:56 +0500

    Added TcpListener

--- pigeon-server/Program.cs
+++ pigeon-server/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,6 +22,8 @@ namespace pigeon_server
             }
             Console.ReadKey();
             // start listener
+
+            TcpListener TcpListener = new TcpListener(55387);
 
 
         }
