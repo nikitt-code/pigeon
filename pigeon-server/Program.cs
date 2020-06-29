c792e1cc7683478e4d00bc9e4394385a7fb041df
kolya5544 <05-kolya@bk.ru>
2020-06-29 11:09:39 +0300

    В процессе разработки диспатчера пакетов

--- pigeon-server/Program.cs
+++ pigeon-server/Program.cs
@@ -12,6 +12,12 @@ namespace pigeon_server
 {
     class Program
     {
+        public struct Packet
+        {
+            public string Filename;
+            public int FileSize;
+            public byte[] FileContents;
+        }
         public static NetworkStream NetworkStream { get; private set; }
 
         static void Main(string[] args)
@@ -27,13 +33,13 @@ namespace pigeon_server
             Console.ReadKey();
             // start listener
 
-            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPAddress ip = IPAddress.Parse("0.0.0.0");
             int port = 55387;
             TcpListener tcpListener = new TcpListener(ip, port);
             tcpListener.Start();
             TcpClient tcpClient = tcpListener.AcceptTcpClient();
             NetworkStream ns = tcpClient.GetStream();
-            ns.ReadTimeout = 36000;
+            ns.ReadTimeout = 300000;
             ns.WriteTimeout = 36000;
             StreamReader sr = new StreamReader(ns);
             StreamWriter sw = new StreamWriter(ns);
@@ -42,10 +48,53 @@ namespace pigeon_server
             Console.Write(" [ Connected ] \r\n");
 
             byte[] buffer = ReadContents(ns);
+            Packet[] files = null;
+            var Result = DispatchPacket(ref files, buffer);
+            if (Result == 0)
+            {
+                Console.Write("  File successfully transferred.\r\n");
+            } else
+            {
+                Console.Write("  Some files were corrupted. Do you still want to save them?\r\n");
+                Console.Write("(Y/N): ");
+                string Answer = Console.ReadLine();
+                switch (Answer)
+                {
+                    case "Y": SaveFiles(files); break;
+                    default: return;
+                }
+            }
 
+        }
 
-            Console.Write("  File transfered.\r\n");
+        private static void SaveFiles(Packet[] files)
+        {
+            
+        }
 
+        private static int DispatchPacket(ref Packet[] files, byte[] buffer)
+        {
+            List<Packet> PacketsDispatched = new List<Packet>();
+            int Position = 0;
+            while (true)
+            {
+                var NewPacket = new Packet();
+                string FileName = ReadNT_UTF8_String(buffer, Position);
+            }
+        }
+
+        public static string ReadNT_UTF8_String(byte[] array, int offset, int limit=128)
+        {
+            if (array.Length <= offset) throw new ArgumentException("Offset is higher than array length");
+            string Output = "";
+            for (int i = offset; i<array.Length; i++)
+            {
+                if (limit < 0) break;
+                limit--;
+                if (array[i] == 0x00) break;
+                Output += (char)array[i];
+            }
+            return Output;
         }
 
         public static byte[] ReadContents(Stream str)
